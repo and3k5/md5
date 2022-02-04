6071bee153058a9e8c7c83a0958344d220f6bde3
Anders Kjeldsen <and3k5@users.noreply.github.com>
2022-02-04 14:19:03 +0100

    work in progress

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ var xtbl = new Uint8Array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
     5, 8, 11, 14, 1, 4, 7, 10, 13, 0, 3, 6, 9, 12, 15, 2,
     0, 7, 14, 5, 12, 3, 10, 1, 8, 15, 6, 13, 4, 11, 2, 9]);
 
-function md5(s) {
+export function md5(s) {
     // CONSTANTS
     var bin, tmp, i;
 
---
+++ webpack.config.js
@@ -0,0 +1,5 @@
+/** @type {import('webpack').Configuration} */
+module.exports = {
+    "$schema": "./node_modules/webpack/schemas/WebpackOptions.json",
+    target: ""
+}
